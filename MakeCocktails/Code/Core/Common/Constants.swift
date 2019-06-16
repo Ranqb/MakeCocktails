a90ea894af42ef96a4c00ccfbfd28550330c77f8
Andrey Nedov <Andrey@MacBook-Air.local>
2019-06-16 23:19:54 +0300

    set icon and product name

--- MakeCocktails/Code/Core/Common/Constants.swift
+++ MakeCocktails/Code/Core/Common/Constants.swift
@@ -25,4 +25,4 @@ let delay = 0.5
 let cornerRadius = CGFloat(8)
 let statusBarStyle: UIStatusBarStyle = .default
 let navBackgroundColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1)
-let navTextColor = #colorLiteral(red: 0.05537921935, green: 0.133256048, blue: 0.2372736037, alpha: 1)
+let navTextColor = #colorLiteral(red: 0.05490196078, green: 0.1333333333, blue: 0.2392156863, alpha: 1)
